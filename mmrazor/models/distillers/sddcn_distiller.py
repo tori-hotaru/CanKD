feb9c6bc7a881a79fce0eb4c8c29c427d343a45a
Alex Sun <58972377+tori-hotaru@users.noreply.github.com>
2025-11-26 15:53:38 +0900

    Delete mmrazor/models/distillers/sddcn_distiller.py

--- mmrazor/models/distillers/sddcn_distiller.py
+++
@@ -1,9 +0,0 @@
-from mmrazor.registry import MODELS
-from .byot_distiller import BYOTDistiller 
-
-
-@MODELS.register_module()
-class SDDCNDistiller(BYOTDistiller): 
-    """SDDCNDistiller inherits BYOTDistiller to reach the goal of self-distillation for SDDCN."""
-    
-    pass
