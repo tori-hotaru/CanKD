692068d5606ecf8e5d9b4ad45c5e5e2cbb9a00be
Alex Sun <58972377+tori-hotaru@users.noreply.github.com>
2025-11-26 17:50:31 +0900

    Add configuration for NLKD with RetinaNet

---
+++ configs/distill/mmdet/nlkd/nlkdD_fpn_retinanet-r101_retinanet-r50_1x_objects365v2.py
@@ -0,0 +1,202 @@
+_base_ = [
+    'mmdet::_base_/datasets/objects365v2_detection.py',
+    'mmdet::_base_/schedules/schedule_1x.py', 'mmdet::_base_/default_runtime.py'
+]
+
+
+teacher_ckpt = '/home/ohym_sun/data/mmrazor/ckpt/epoch_12.pth'
+
+teacher = dict(
+    _scope_ = 'mmdet',
+    type='RetinaNet',
+    init_cfg=dict(type='Pretrained', checkpoint=teacher_ckpt),
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_size_divisor=32),
+    backbone=dict(
+        type='ResNet',
+        depth=101,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_input',
+        num_outs=5),
+    bbox_head=dict(
+        type='RetinaHead',
+        num_classes=365,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            octave_base_scale=4,
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],
+            strides=[8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    # model training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.4,
+            min_pos_iou=0,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='PseudoSampler'),  # Focal loss should use PseudoSampler
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms', iou_threshold=0.5),
+        max_per_img=100))
+
+student_cfg = dict(
+    _scope_ = 'mmdet',
+    type='RetinaNet',
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_size_divisor=32),
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        start_level=1,
+        add_extra_convs='on_input',
+        num_outs=5,
+        init_cfg=dict(type='Pretrained', prefix='neck.', checkpoint=teacher_ckpt)),
+    bbox_head=dict(
+        type='RetinaHead',
+        num_classes=365,
+        in_channels=256,
+        stacked_convs=4,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            octave_base_scale=4,
+            scales_per_octave=3,
+            ratios=[0.5, 1.0, 2.0],
+            strides=[8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0),
+        init_cfg=dict(type='Pretrained', prefix='bbox_head.', checkpoint=teacher_ckpt)),
+    # model training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.4,
+            min_pos_iou=0,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='PseudoSampler'),  # Focal loss should use PseudoSampler
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms', iou_threshold=0.5),
+        max_per_img=100))
+
+model = dict(
+    _scope_ = 'mmrazor', 
+    type='FpnTeacherDistill',
+    architecture=student_cfg,
+    teacher=teacher,
+    distiller=dict(
+        type='ConfigurableDistiller',
+        student_recorders=dict(fpn=dict(type='ModuleOutputs', source='neck')),
+        teacher_recorders=dict(fpn=dict(type='ModuleOutputs', source='neck')),
+        distill_losses=dict(
+            loss_nlkd_fpn0=dict(type='NLKD_IN_Loss', in_channels=256, dimension=2, loss_weight=10),
+            loss_nlkd_fpn1=dict(type='NLKD_IN_Loss', in_channels=256, dimension=2, loss_weight=10),
+            loss_nlkd_fpn2=dict(type='NLKD_IN_Loss', in_channels=256, dimension=2, loss_weight=10),
+            loss_nlkd_fpn3=dict(type='NLKD_IN_Loss', in_channels=256, dimension=2, loss_weight=10),
+        ),  
+        loss_forward_mappings=dict(
+            loss_nlkd_fpn0=dict(s_feature=dict(from_student=True, recorder='fpn', data_idx=0), t_feature=dict(from_student=False, recorder='fpn', data_idx=0)),
+            loss_nlkd_fpn1=dict(s_feature=dict(from_student=True, recorder='fpn', data_idx=1), t_feature=dict(from_student=False, recorder='fpn', data_idx=1)),
+            loss_nlkd_fpn2=dict(s_feature=dict(from_student=True, recorder='fpn', data_idx=2), t_feature=dict(from_student=False, recorder='fpn', data_idx=2)),
+            loss_nlkd_fpn3=dict(s_feature=dict(from_student=True, recorder='fpn', data_idx=3), t_feature=dict(from_student=False, recorder='fpn', data_idx=3)),
+        )
+    )
+)
+
+find_unused_parameters = True   
+val_cfg = dict(_delete_=True, type='mmrazor.SingleTeacherDistillValLoop')
+
+# Using 4 GPUS while training
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001),
+    clip_grad=dict(max_norm=35, norm_type=2))
+
+# learning rate
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=1.0 / 1000,
+        by_epoch=False,
+        begin=0,
+        end=10000),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=12,
+        by_epoch=True,
+        milestones=[8, 11],
+        gamma=0.1)
+]
+
+# NOTE: `auto_scale_lr` is for automatically scaling LR,
+# USER SHOULD NOT CHANGE ITS VALUES.
+# base_batch_size = (8 GPUs) x (2 samples per GPU)
+auto_scale_lr = dict(base_batch_size=16)
