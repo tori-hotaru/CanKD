14d04ee041c49e9f5642c0d3bde3720975ad1866
Alex Sun <58972377+tori-hotaru@users.noreply.github.com>
2025-11-26 20:39:09 +0900

    Remove SDDCNDistiller from __all__ exports
    Removed SDDCNDistiller from the module exports.

--- mmrazor/models/distillers/__init__.py
+++ mmrazor/models/distillers/__init__.py
@@ -3,7 +3,6 @@ from .base_distiller import BaseDistiller
 from .byot_distiller import BYOTDistiller
 from .configurable_distiller import ConfigurableDistiller
 from .ofd_distiller import OFDDistiller
-from .sddcn_distiller import SDDCNDistiller
 __all__ = [
-    'ConfigurableDistiller', 'BaseDistiller', 'BYOTDistiller', 'OFDDistiller', 'SDDCNDistiller'
+    'ConfigurableDistiller', 'BaseDistiller', 'BYOTDistiller', 'OFDDistiller'
 ]
